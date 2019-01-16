9dcc6d2a84149cd1ac0e2dec6eafcb322e1af5c5
Eric Bower <neurosnap@gmail.com>
2019-01-16 01:04:45 -0500

    payload is optiona;

--- src/action.ts
+++ src/action.ts
@@ -1,10 +1,16 @@
-import { ActionType } from './types';
+type Creator<P = never> = (p?: P) => { type: string; payload?: P };
 
-export default function creator<P = any>(type: ActionType) {
-  const action = (payload: P) => ({
-    type,
-    payload,
-  });
+export default function creator<P = never>(type: string): Creator<P> {
+  function action(payload?: P) {
+    if (!payload) {
+      return { type };
+    }
+
+    return {
+      type,
+      payload,
+    };
+  }
 
   action.toString = () => `${type}`;
   return action;
