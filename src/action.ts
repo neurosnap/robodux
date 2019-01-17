be2ef9731a775d5ba8b36c6b79c1a399b1f51189
Eric Bower <neurosnap@gmail.com>
2019-01-17 10:20:56 -0500

    :bug: allow createAction to have no payload

--- src/action.ts
+++ src/action.ts
@@ -1,8 +1,16 @@
-export default function creator<P = any>(type: string) {
-  const action = (payload: P) => ({
-    type,
-    payload,
-  });
+type Creator<P = never> = (p?: P) => { type: string; payload?: P };
+
+export default function creator<P = never>(type: string): Creator<P> {
+  function action(payload?: P) {
+    if (!payload) {
+      return { type };
+    }
+
+    return {
+      type,
+      payload,
+    };
+  }
 
   action.toString = () => `${type}`;
   return action;
