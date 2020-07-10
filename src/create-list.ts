c4c259e549fb4c6ab3f3dece8736253f5b9e40fc
Eric Bower <neurosnap@gmail.com>
2020-07-10 14:24:29 -0400

    List slice (#41)
    * createList slice helper
    
    * update doc

---
+++ src/create-list.ts
@@ -0,0 +1,42 @@
+import createSlice from './create-slice';
+import { SliceHelper } from './types';
+
+export function listReducers<State extends any[]>(
+  initialState = ([] as unknown) as State,
+) {
+  return {
+    add: (state: State, payload: State): State => {
+      return [...state, ...payload] as State;
+    },
+    remove: (state: State, payload: number[]): State => {
+      const newState = [...state] as State;
+      payload.forEach((index) => {
+        newState.splice(index, 1);
+      });
+      return newState;
+    },
+    reset: (state: State): State => initialState,
+  };
+}
+
+interface MapActions<S> {
+  add: S;
+  remove: number[];
+  reset: never;
+}
+
+export default function createList<State extends any[]>({
+  name,
+  extraReducers,
+  initialState = ([] as unknown) as State,
+}: SliceHelper<State>) {
+  const slice = createSlice<State, MapActions<State>>({
+    name,
+    initialState,
+    extraReducers,
+    useImmer: false,
+    reducts: listReducers(initialState),
+  });
+
+  return slice;
+}
