32f69c04280abb7a638870de02007df40d4a3765
Eric Bower <me@erock.io>
2021-12-22 13:22:03 -0500

    upgrade immer

--- src/create-reducer.ts
+++ src/create-reducer.ts
@@ -1,4 +1,4 @@
-import createNextState from 'immer';
+import createNextState, { Draft } from 'immer';
 
 import { Action } from './types';
 import { ReducerMap } from './types';
@@ -29,13 +29,16 @@ export default function createReducer<State = any>(
   initialState: State,
   reducers: ReducerMap<State>,
 ): Reducer<State, Action<any>> {
-  const reducer = (state = initialState, action: Action<any>) => {
-    return createNextState<State>(state, (draft) => {
+  const reducer = (state = initialState, action: Action<any>): State => {
+    // @ts-ignore createNextState() produces an Immutable<Draft<S>> rather
+    // than an Immutable<S>, and TypeScript cannot find out how to reconcile
+    // these two types.
+    return createNextState(state, (draft: Draft<State>) => {
       const caseReducer = reducers[action.type];
       if (!caseReducer) {
         return draft;
       }
-      return caseReducer(draft as State, action.payload);
+      return caseReducer(draft as any, action.payload);
     });
   };
 
